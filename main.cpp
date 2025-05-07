49c18adf218a7df698da148813dac065d6a46948
Ueno02012 <k023g0064@g.neec.ac.jp>
2025-05-07 10:46:47 +0900

    test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <Windows.h>
+#include "KamataEngine.h"
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
+KamataEngine::Initialize(L"LE3D_02_ウエノ_ユウキ");
+
+// DirectXCommonのインスタンスの取得
+DirectXCommon* dxcommon = DirectXCommon::GetInstance();
+
+//メインループ
+while (true) {
+	//エンジンの更新
+	if (KamataEngine::Update()) {
+		break;
+	}
+	//描画開始
+	dxcommon->PreDraw();
+
+	dxcommon->PostDraw();
+}
+//エンジンの終了処理
+KamataEngine::Finalize();
+
 	return 0;
 }
