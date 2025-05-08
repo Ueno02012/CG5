4bf6903136a901c4c35fadf729e0a6d41ca4fc41
Ueno02012 <k023g0064@g.neec.ac.jp>
2025-05-08 10:00:58 +0900

    test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include "KamataEngine.h"
+#include <cassert>
 using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -8,18 +9,71 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 KamataEngine::Initialize(L"LE3D_02_ウエノ_ユウキ");
 
 // DirectXCommonのインスタンスの取得
-DirectXCommon* dxcommon = DirectXCommon::GetInstance();
+DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
-//メインループ
+// DirectXCommonクラスが取得している、ウィンドウの幅と高さの値の取得
+int32_t w = dxCommon->GetBackBufferWidth();
+int32_t h = dxCommon->GetBackBufferHeight();
+DebugText::GetInstance()->ConsolePrintf(
+	std::format("width: {},height: {}\n", w, h).c_str());
+
+// DirectXCommonクラスが管理している、コマンドリストの取得
+
+ID3D12GraphicsCommandList* commandList = dxCommon->GetCommandList(); 
+
+
+//-----------------RootSignatureの作成------------------//
+//構造体にデータを用意する
+D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
+descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+ID3DBlob* signatureBlob = nullptr;
+ID3DBlob* errorBlog = nullptr;
+HRESULT hr = D3D12SerializeRootSignature(&descriptionRootSignature,
+	D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlog);
+if (FAILED(hr)) {
+	DebugText::GetInstance()->ConsolePrintf(
+		reinterpret_cast<char*>(errorBlog->GetBufferPointer()));
+	assert(false);
+}
+// バイナリを元に生成
+ID3D12RootSignature* rootSignature = nullptr;
+hr = dxCommon->GetDevice()->CreateRootSignature(
+	0, signatureBlob->GetBufferPointer(), signatureBlob->GetBufferSize(),
+	IID_PPV_ARGS(&rootSignature));
+assert(SUCCEEDED(hr));
+
+//-------------------InPutLayoutの設定------------------//
+D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
+inputElementDescs[0].SemanticName = "POSITION";
+inputElementDescs[0].SemanticIndex = 0;
+inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
+inputLayoutDesc.pInputElementDescs = inputElementDescs;
+inputLayoutDesc.NumElements = _countof(inputElementDescs);
+
+//-------------------BlendStateの設定-------------------//
+D3D12_BLEND_DESC blendDesc{};
+// 全ての色要素を書き込む
+blendDesc.RenderTarget[0].RenderTargetWriteMask =
+D3D12_COLOR_WRITE_ENABLE_ALL;
+
+//------------------RasterizerStateの設定-----------------//
+D3D12_RASTERIZER_DESC rasterizerDesc{};
+// 裏面(反時計回り)をカリングする
+rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
+
+
+// メインループ
 while (true) {
 	//エンジンの更新
 	if (KamataEngine::Update()) {
 		break;
 	}
 	//描画開始
-	dxcommon->PreDraw();
+	dxCommon->PreDraw();
 
-	dxcommon->PostDraw();
+	dxCommon->PostDraw();
 }
 //エンジンの終了処理
 KamataEngine::Finalize();
