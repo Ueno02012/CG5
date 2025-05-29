f605a319131b9c3ad31f0cd5245cb6276a66ee40
Ueno02012 <k023g0064@g.neec.ac.jp>
2025-05-29 10:16:05 +0900

    完了

--- GameScene.cpp
+++ GameScene.cpp
@@ -1,4 +1,8 @@
 #include "GameScene.h"
+#include <random>
+std::random_device seedGenerator;
+std::mt19937 randomEngine(seedGenerator());
+std::uniform_real_distribution<float> distribution(-1.0f, 1.0f);
 
 GameScene::GameScene() {}
 
@@ -21,9 +25,12 @@ void GameScene::Initialize() {
 	for (int i = 0; i < 150; i++) {
 		Particle* particle = new Particle();
 		// 位置
-		Vector3 position = {0.5f * i, 0.0f, 0.0f};
+		Vector3 position = {0.0f, 0.0f, 0.0f};
+		// 移動量
+		Vector3 velocity = {distribution(randomEngine), distribution(randomEngine), 0};
+
 		// 初期化
-		particle->Initialize(modelParticle_, position);
+		particle->Initialize(modelParticle_, position,velocity);
 		//　リストに追加
 		particles_.push_back(particle);
 
--- Particle.cpp
+++ Particle.cpp
@@ -3,12 +3,14 @@
 using namespace MathUtility;
 
 
-void Particle::Initialize(Model* model,Vector3 position) { 
+void Particle::Initialize(Model* model,Vector3 position,Vector3 velocity) { 
 	assert(model);
 
 	model_ = model;
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
+	velocity_ = velocity;
+	worldTransform_.scale_ = {0.2f, 0.2f, 0.2f};
 
 	// 色の設定
 	objectColor_.Initialize();
@@ -16,7 +18,7 @@ void Particle::Initialize(Model* model,Vector3 position) {
 }
 
 void Particle::Update() { 
-	worldTransform_.translation_ += {0.0f, 0.1f, 0.0f};
+	worldTransform_.translation_ += velocity_;
 	worldTransform_.UpdateMatrix();
 
 	// 色変更に数値を設定
